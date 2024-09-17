5f5f57ac5d52e4bf9fa88b405002d1e571ec927c
Samir <samir.ibrahim.daily@gmail.com>
2024-09-17 20:59:10 +0200

    Streamlit lecture

---
+++ code_alongs/03_streamlit_intro/dashboard.py
@@ -0,0 +1,18 @@
+import streamlit as st 
+import pandas as pd 
+from pathlib import Path
+import plotly.express as px 
+
+def read_data():
+    data_path = Path(__file__).parents[2] / "data"
+    df = pd.read_csv(data_path / "cleaned_yh_region.csv",index_col=0, parse_dates=[0])
+    df.index = df.index.year
+    return df 
+
+print(read_data())
+
+def layout():
+    st.markdown("# Dashboard YH")
+
+if __name__ == '__main__':
+    layout()
