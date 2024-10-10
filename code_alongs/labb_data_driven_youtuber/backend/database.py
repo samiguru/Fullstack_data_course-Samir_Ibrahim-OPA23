d95154f4f6b23061c1aa2401cabcf5b950b051d3
Samir <samir.ibrahim.daily@gmail.com>
2024-10-10 19:46:19 +0200

    database

---
+++ code_alongs/labb_data_driven_youtuber/backend/database.py
@@ -0,0 +1,27 @@
+import duckdb
+
+
+class Database:
+
+    def __init__(self, db_path) -> None:
+        self.db_path = db_path
+        self.connection = None
+
+
+    def __enter__(self):
+        self.connection = duckdb.connect(self.db_path)
+        return self
+
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.connection:
+            self.connection.close()
+
+
+    def query(self, query):
+        return self.connection.execute(query).fetchall()
+   
+
+class DatabaseDataFrame(Database):
+    def query(self, query):
+        return self.connection.execute(query).df()
