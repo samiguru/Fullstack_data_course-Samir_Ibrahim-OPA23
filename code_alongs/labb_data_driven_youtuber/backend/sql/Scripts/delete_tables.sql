db3ea521e471a4318a2ae059e875b3f56c04bc8d
Samir <samir.ibrahim.daily@gmail.com>
2024-10-11 20:50:27 +0200

    the fast delete table tool added

---
+++ code_alongs/labb_data_driven_youtuber/backend/sql/Scripts/delete_tables.sql
@@ -0,0 +1,23 @@
+--DROP TABLE marts.content_view_time;
+
+--DROP TABLE marts.device_overview;
+
+--DROP TABLE marts.subscribers;
+
+--DROP TABLE marts.viewer_content_geografy;
+
+--DROP TABLE marts.views_device_per_date;
+
+--DROP TABLE marts.views_latest_10;
+
+--DROP TABLE marts.views_per_age;
+
+--DROP TABLE marts.views_per_date;
+
+--DROP TABLE marts.views_per_gender;
+
+--DROP TABLE marts.views_per_op_system;
+
+--DROP TABLE marts.views_top_10;
+
+SELECT * FROM information_schema.tables WHERE table_schema = 'marts';
