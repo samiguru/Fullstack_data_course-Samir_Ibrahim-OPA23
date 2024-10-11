ce9237525c92997d58bbe50d1337752a72e76cd5
Samir <samir.ibrahim.daily@gmail.com>
2024-10-11 20:46:39 +0200

    dashboard completed and tested

--- code_alongs/labb_data_driven_youtuber/dashboard.py
+++ code_alongs/labb_data_driven_youtuber/dashboard.py
@@ -1,29 +1,56 @@
-import streamlit as st 
-from frontend.kpi import ContentKPI
+import streamlit as st
+from frontend.kpi import ContentKPI, GenderAgeKPI, ViewerGeografy, OpSystemViews, Subscribers, Top10
 from frontend.graphs import ViewsTrend
 
 
-# device_kpi = DeviceKPI()
+
 content_kpi = ContentKPI()
 views_graph = ViewsTrend()
+age_kpi = GenderAgeKPI()
+viewer_geografy = ViewerGeografy()
+viewer_geografy_plot = ViewsTrend()
+op_system = OpSystemViews()
+op_system_plot = ViewsTrend()
+subscribers = Subscribers()
+top_10 = Top10()
+top_10_plot = ViewsTrend()
+latest_10_plot = ViewsTrend()
+
 
 def layout():
-    st.markdown("# The data driven youtuber")
-    st.markdown("Den här dashboarden syftar till att utforska datan i min youtubekanal")
-    # device_kpi.display_device_views()
-    # device_kpi.display_device_summary()
-    
+
     st.sidebar.title("Meny")
     page = st.sidebar.radio(
         label="Välj sida",
-        options=["Översikt", "Tittare"])
-    
-    if page == "Översikt":
+        options=["Översikt", "Tittare", "Trender och nya releaser", "Enhetstyp"])
+
+    if page == "Översikt": # Första sidan
         st.markdown("## KPIer för videor")
         st.markdown("Nedan visas sammanfattning för totala antal")
-    
         content_kpi.display_content()
         views_graph.display_plot()
+    
+
+    elif page == "Tittare": # Andra sidan
+        st.markdown("## Tittarstatistik")
+        age_kpi.display_gender_age()
+        viewer_geografy.display_viewer_geografy()
+        viewer_geografy_plot.geografy_map_plot()
+        subscribers.display_subscribers()
+
+
+    elif page == "Trender och nya releaser": # Tredje sidan
+        st.header("Videostatistik och trender")
+        top_10_plot.top_10_plot()
+        top_10.display_top_10()
+        latest_10_plot.latest_10_plot()
+
+
+    elif page == "Enhetstyp": # Fjärde sidan
+        st.header("Statistik")
+        op_system_plot.op_system_plot()
+        op_system.display_op_system()
+
 
 if __name__ == "__main__":
     layout()
