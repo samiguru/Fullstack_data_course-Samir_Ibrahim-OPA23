905216ab71f521c6706f7ddd0bbbe50d8df06188
Samir <samir.ibrahim.daily@gmail.com>
2024-10-11 20:50:01 +0200

    the dounble-check tool view_tables added

---
+++ code_alongs/labb_data_driven_youtuber/backend/sql/Scripts/view_tables.sql
@@ -0,0 +1,23 @@
+SELECT * FROM information_schema.tables WHERE table_schema = 'marts';
+
+SELECT * FROM marts.content_view_time;
+
+SELECT * FROM marts.device_overview;
+
+SELECT * FROM marts.subscribers;
+
+SELECT * FROM marts.viewer_content_geografy;
+
+SELECT * FROM marts.views_device_per_date;
+
+SELECT * FROM marts.views_latest_10;
+
+SELECT * FROM marts.views_per_age;
+
+SELECT * FROM marts.views_per_date;
+
+SELECT * FROM marts.views_per_gender;
+
+SELECT * FROM marts.views_per_op_system;
+
+SELECT * FROM marts.views_top_10;
